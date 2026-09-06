3128a62aa72ed0e0d0ab9b504c42d0c29875be60
Nimoh Kariuki <nimohkariuki9@gmail.com>
2026-09-06 22:08:34 +0300

    Update PublicationEditor.js

--- kenya-womens-march/src/components/PublicationEditor.js
+++ kenya-womens-march/src/components/PublicationEditor.js
@@ -201,7 +201,7 @@ const PublicationEditor = () => {
       setUploadProgress('');
       setSelectedFile(null);
       
-      // Show detailed error in notification
+      // Show detailed error in notification 
       setNotification({ 
         message: errorMessage, 
         type: 'error' 
